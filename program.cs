e6ad21f691ad3b50a79f64934179b58b9bc3258a
joe12134 <144343524+joe12134@users.noreply.github.com>
2023-09-12 13:39:08 +0100

    Update program.cs

--- program.cs
+++ program.cs
@@ -1,1 +1,30 @@
 
+using System;
+
+namespace Glazing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = 0;
+            double height = 0;
+
+            const double max_width = 5.00;
+
+            Console.WriteLine("enter width of window");
+
+            width = double.Parse(Console.ReadLine());  
+
+            if (width > max_width)
+            {
+                Console.WriteLine("Too wide");
+
+            } else
+            {
+                Console.WriteLine("Good");
+            }
+
+        }
+    }
+}
